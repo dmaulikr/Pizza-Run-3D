6b18db777ba6c8751233203773b08a176d83d168
Renato Tramontano <kingborn187@imac.local>
2017-08-17 19:42:38 +0200

    Commit per gennaro

--- Pizza Run 3D/ViewController.swift
+++ Pizza Run 3D/ViewController.swift
@@ -8,14 +8,11 @@ class ViewController: UIViewController {
     
     var scnView: SCNView!
     var gameScene: SCNScene!
-    var street1Node: SCNNode!
-    var street2Node: SCNNode!
+    var streetNode: SCNNode!
     var playerNode: SCNNode!
     var cityNode: SCNNode!
     var grass1Node: SCNNode!
     var grass2Node: SCNNode!
-    var gallery1Node: SCNNode!
-    var gallery2Node: SCNNode!
     
     var movePlayerUpAction: SCNAction!
     var movePlayerDownAction: SCNAction!
@@ -34,19 +31,16 @@ class ViewController: UIViewController {
         self.view.addSubview(scnView)
         gameScene = SCNScene(named: "PizzaRun3D.scnassets/Scenes/gameScene.scn")
         scnView.scene = gameScene
-        scnView.showsStatistics = true
-        scnView.allowsCameraControl = true
+//        scnView.showsStatistics = true
+//        scnView.allowsCameraControl = true
     }
     
     func setupNodes() {
-        street1Node = gameScene.rootNode.childNode(withName: "strada_1", recursively: true)!
-        street2Node = gameScene.rootNode.childNode(withName: "strada_2", recursively: true)!
+        streetNode = gameScene.rootNode.childNode(withName: "strada", recursively: true)!
         playerNode = gameScene.rootNode.childNode(withName: "personaggio", recursively: true)!
         cityNode = gameScene.rootNode.childNode(withName: "citta", recursively: true)!
         grass1Node = gameScene.rootNode.childNode(withName: "erba_1", recursively: true)!
         grass2Node = gameScene.rootNode.childNode(withName: "erba_2", recursively: true)!
-        gallery1Node = gameScene.rootNode.childNode(withName: "galleria_1", recursively: true)!
-        gallery2Node = gameScene.rootNode.childNode(withName: "galleria_2", recursively: true)!
     }
     
     func setupActions() {
@@ -56,15 +50,8 @@ class ViewController: UIViewController {
         cityNode.runAction(moveCityAction)
         
         // Move Street1 and Street2
-        let moveStreet1Action = SCNAction.moveBy(x: -16, y: 0, z: 0, duration: 2)
-        let moveStreet2Action = SCNAction.moveBy(x: -31.9, y: 0, z: 0, duration: 4)
-        let reposeStreetAction = SCNAction.move(to: SCNVector3(x: 13.9, y: 0.147, z: 2.315), duration: 0)
-        
-        let street1Action = SCNAction.sequence([moveStreet1Action, reposeStreetAction])
-        let street2Action = SCNAction.sequence([moveStreet2Action, reposeStreetAction])
-        
-        street1Node.runAction(SCNAction.sequence([street1Action, SCNAction.repeat(street2Action, count: 1)]))
-        street2Node.runAction(SCNAction.repeat(street2Action, count: 2))
+        let moveStreetAction = SCNAction.moveBy(x: -550, y: 0, z: 0, duration: 60)
+        streetNode.runAction(moveStreetAction)
         
         // Move Player
         let inclineScouterLeftAction = SCNAction.rotateBy(x: -0.5, y: 0, z: 0, duration:0)
@@ -85,21 +72,6 @@ class ViewController: UIViewController {
       
         grass1Node.runAction(SCNAction.sequence([grass1Action, SCNAction.repeatForever(grass2Action)]))
         grass2Node.runAction(SCNAction.repeatForever(grass2Action))
-        
-        // Move Gallery1 and Gallery2
-        let moveGallery1Action = SCNAction.moveBy(x: -44, y: 0, z: 0, duration: 5.5)
-        let moveGallery2Action = SCNAction.moveBy(x: -52, y: 0, z: 0, duration: 6.5)
-        let moveGallery3Action = SCNAction.moveBy(x: -19, y: 0, z: 0, duration: 2.5)
-        let reposeGalleryAction = SCNAction.move(to: SCNVector3(x: 8, y: -0.229, z: 3.18), duration: 0)
-
-        let gallery1Action = SCNAction.sequence([moveGallery1Action, reposeGalleryAction])
-        let gallery2Action = SCNAction.sequence([moveGallery2Action, reposeGalleryAction])
-
-        gallery1Node.runAction(SCNAction.sequence([SCNAction.wait(duration: 4), gallery1Action, moveGallery3Action]))
-        gallery2Node.runAction(SCNAction.sequence([SCNAction.wait(duration: 4), moveGallery2Action]))
-//        gallery1Node.runAction(SCNAction.sequence([gallery1Action, SCNAction.repeat(gallery2Action, count: 4)]))
-//        gallery2Node.runAction(SCNAction.repeat(gallery2Action, count: 5))
-
     }
     
     func setupGesture() {
